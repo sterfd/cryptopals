56e324d2a5f69e8f5cce55f1253dd30a79525deb
sterf-mac <stefhwin@gmail.com>
2024-02-06 14:00:47 -0500

    decoding ctf ciphertext

---
+++ ctf-test.py
@@ -0,0 +1,10 @@
+ciphers = [
+    "NEaYuBwJQw!3euv-QsHThefYp93iVRweplQqOuRwgRAi32yPBPRi8eFzmDKvyA4nHWQ!mXzXfLs!xQhdjD-tvagogbTl2Q3qYaB9K5p5kBBxH4LD35hFFvtba5MQjmV0O7-sGDinfUyXle2Pw8FhvbieB6PcxuSQtZDrOjwGgC0UP8JUb9qOXsZOfcQIshQfozUiiA4CX5phOHKWPoLBOQ~~",
+    "GXbQ4PmxKdsQIe3zvmF8ZRH9uE2rnzCAzcz2pHmnIf2NQvIlYuRCzpo1TtX-22RIrJScJgX3UYY1HN0-YB5UfCyy9tbUlJADPAvlopfz4s3NaIfbYKe9C4nHhkDbOrSGzAfOAi!XQQFQ5YojEtDxbtzdGoSMpoiVTvLzcFYw5PsCRI33xSsHkXydcI!K6Z53p2Y4L9mZeH7Ik2Qh5JaMyQ~~",
+    "V9FHocpFo0gufAoV!KI6q4RiPmg3R29EllvNSAGFOl11bfxywlekDgVzSl0Yg0UMjHZg9XFnuyWuMM9bvGzFHH!Yznru2riRZ66cDIztq1wIStlORb2soBmlD4e!9W2CObqS!pVnABw6ZsAOMDns4lIojJqWkeMewXv6jR4CR7myXO3V8HpcST9sgGMw84-TZzfx-Romci5z5Bp!!H1KUw~~",
+    "NaQZFcriQXhWOVqybFQ6!3VHVoW28eO6ZRlhK!vmoCefjEH3060Lxi2M5N2xHh22tKRwcqj!QJ3v2Y3S4875F2GW-upZ0Ah!jZ0PVsKofobWKxqidIvNw7hrgZIoLbPomKJPsUhUcwQRiSgq0RD8f1h!zVtlnhNOIBvDlQAWKsdJgLp1T45zta9kg-FBX4gE2MEC4pV44MaNYmquYOKI!w~~",
+    "!egHgLqmk!6JMJ7I2JpyVyUGRD7Ute1A6IfQfvS!-jC4dV!E35MJdTqvzEfXZSqywndLpURcQ230E1LtFH-DUBlY0OwZbmtQGzsF6NTVutompOONHok-E2WR!lXDH9iyh2Zt7jsal91kT-kJ77m4zQZ0AwFqP6r-cYzM7c1E9hSLsW5aWC6DuHZ5rdNHfivRvddiUA-8EKaZA-gPxu3btw~~",
+]
+
+for c in ciphers:
+    print(len(c), c.count("!"), c.count("~"))
